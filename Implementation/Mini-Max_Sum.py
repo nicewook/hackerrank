703f394d4b80de53de4269da27479ccddf2fd143
JEONG HYUN SEOK <nicewook@hotmail.com>
2016-12-23 18:21:47 +0900

    Update Mini-Max_Sum.py

--- Implementation/Mini-Max_Sum.py
+++ Implementation/Mini-Max_Sum.py
@@ -1,1 +1,8 @@
-# Mini-Max Sum
+#!/bin/python
+# Mini-Max Sum: https://www.hackerrank.com/challenges/mini-max-sum
+
+import sys
+
+lst = [(int)(x) for x in input().strip().split(' ')]
+x = sum(lst)
+print (x- max(lst), x-min(lst))
